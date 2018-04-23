f48838e7f8a31a63dca04f7e3a783800599c6fb5
Andreas Wälchli <andreas.waelchli@me.com>
2018-04-23 22:05:49 +0200

    #17: added default user test/password that will be added to the in-memory DB on server start. Should probably remain there until we setup a persistent DB

--- src/main/java/ch/pitaya/pitaya/PitayaApplication.java
+++ src/main/java/ch/pitaya/pitaya/PitayaApplication.java
@@ -1,7 +1,13 @@
 package ch.pitaya.pitaya;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import ch.pitaya.pitaya.model.User;
+import ch.pitaya.pitaya.repository.UserRepository;
 
 @SpringBootApplication
 public class PitayaApplication {
@@ -9,5 +15,12 @@ public class PitayaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PitayaApplication.class, args);
 	}
-	
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepo, PasswordEncoder bcrypt) {
+		return args -> {
+			userRepo.save(new User("Test User", "test", "test@test.com", bcrypt.encode("password")));
+		};
+	}
+
 }
