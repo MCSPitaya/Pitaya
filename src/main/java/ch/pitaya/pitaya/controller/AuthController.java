ce3da22c0063bee8c9b2ad936b57ec70f5dcd315
Mathias Birrer <matbirre@student.ethz.ch>
2018-04-25 18:24:08 +0200

    Add CORS Support for development environment

--- src/main/java/ch/pitaya/pitaya/controller/AuthController.java
+++ src/main/java/ch/pitaya/pitaya/controller/AuthController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import ch.pitaya.pitaya.security.Token;
 import ch.pitaya.pitaya.service.AuthService;
 import ch.pitaya.pitaya.service.UserService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
--- src/main/java/ch/pitaya/pitaya/controller/UserController.java
+++ src/main/java/ch/pitaya/pitaya/controller/UserController.java
@@ -1,12 +1,14 @@
 package ch.pitaya.pitaya.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.pitaya.pitaya.payload.UserSummary;
 import ch.pitaya.pitaya.security.PrincipalFacade;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class UserController {
 
