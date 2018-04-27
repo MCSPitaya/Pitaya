e6d4cd730ba658d70c2e9a480ee0e863f93f9389
Andreas Wälchli <andreas.waelchli@me.com>
2018-04-27 10:48:09 +0200

    Revert "Add CORS Support for development environment"
    This reverts commit ce3da22c0063bee8c9b2ad936b57ec70f5dcd315.

--- src/main/java/ch/pitaya/pitaya/controller/AuthController.java
+++ src/main/java/ch/pitaya/pitaya/controller/AuthController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +20,6 @@ import ch.pitaya.pitaya.security.Token;
 import ch.pitaya.pitaya.service.AuthService;
 import ch.pitaya.pitaya.service.UserService;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
--- src/main/java/ch/pitaya/pitaya/controller/HeartbeatController.java
+++ src/main/java/ch/pitaya/pitaya/controller/HeartbeatController.java
@@ -1,13 +1,11 @@
 package ch.pitaya.pitaya.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.pitaya.pitaya.payload.ApiResponse;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class HeartbeatController {
 
