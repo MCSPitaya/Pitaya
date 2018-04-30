3e0e88a0f3853ed8576773cb05d56ff172cde1d0
Andreas Wälchli <andreas.waelchli@me.com>
2018-04-30 15:59:49 +0200

    Added barebones DB objects

---
+++ src/main/java/ch/pitaya/pitaya/model/FileData.java
@@ -0,0 +1,42 @@
+package ch.pitaya.pitaya.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class FileData {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@ManyToOne(targetEntity=File.class)
+	private File file;
+
+	protected FileData() {
+		// JPA
+	}
+
+	public FileData(File file) {
+		this.file = file;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+}
