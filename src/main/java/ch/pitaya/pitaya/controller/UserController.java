7ef07e8853e566b7b82866d3c844f1d19ae992fb
Andreas Wälchli <andreas.waelchli@me.com>
2018-05-01 23:23:20 +0200

    added support for firm/case/file notifications

--- src/main/java/ch/pitaya/pitaya/controller/UserController.java
+++ src/main/java/ch/pitaya/pitaya/controller/UserController.java
@@ -1,7 +1,5 @@
 package ch.pitaya.pitaya.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.pitaya.pitaya.model.Notification;
 import ch.pitaya.pitaya.model.User;
 import ch.pitaya.pitaya.payload.request.ChangePasswordRequest;
-import ch.pitaya.pitaya.payload.response.NotificationResponse;
 import ch.pitaya.pitaya.payload.response.SimpleResponse;
 import ch.pitaya.pitaya.payload.response.UserSummary;
 import ch.pitaya.pitaya.security.SecurityFacade;
 import ch.pitaya.pitaya.service.NotificationService;
 import ch.pitaya.pitaya.service.UserService;
-import ch.pitaya.pitaya.util.Utils;
 
 @RequestMapping("/api/user")
 @RestController
@@ -47,15 +41,6 @@ public class UserController {
 		User user = securityFacade.getCurrentUser();
 		userService.changePassword(user, request);
 		return SimpleResponse.ok("password changed");
-	}
-
-	@GetMapping("/changes")
-	public List<NotificationResponse> getNotifications(//
-			@RequestParam(name = "page", defaultValue = "0") int page,
-			@RequestParam(name = "size", defaultValue = "0") int size) {
-		User user = securityFacade.getCurrentUser();
-		List<Notification> notifications = notificationService.getUserNotifications(user, page, size);
-		return Utils.map(notifications, NotificationResponse::new);
 	}
 
 }
