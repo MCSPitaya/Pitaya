66efce50e1ece6fa7912f8065cb0ed51f70da330
Andreas Wälchli <andreas.waelchli@me.com>
2018-05-02 21:39:32 +0200

    added a service that allows users to get user info about all members of the same firm

--- src/main/java/ch/pitaya/pitaya/controller/UserController.java
+++ src/main/java/ch/pitaya/pitaya/controller/UserController.java
@@ -5,15 +5,18 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.pitaya.pitaya.exception.ResourceNotFoundException;
 import ch.pitaya.pitaya.model.User;
 import ch.pitaya.pitaya.payload.request.ChangePasswordRequest;
 import ch.pitaya.pitaya.payload.response.SimpleResponse;
 import ch.pitaya.pitaya.payload.response.UserSummary;
+import ch.pitaya.pitaya.repository.UserRepository;
 import ch.pitaya.pitaya.security.SecurityFacade;
 import ch.pitaya.pitaya.service.UserService;
 
@@ -27,6 +30,9 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
+	@Autowired
+	UserRepository userRepository;
+
 	@GetMapping("/info")
 	public UserSummary getCurrentUser() {
 		return new UserSummary(securityFacade.getCurrentUser());
@@ -37,6 +43,15 @@ public class UserController {
 		User user = securityFacade.getCurrentUser();
 		userService.changePassword(user, request);
 		return SimpleResponse.ok("password changed");
+	}
+
+	@GetMapping("/{id}/info")
+	public UserSummary getUserInfo(@PathVariable("id") long userId) {
+		Long myFirm = securityFacade.getCurrentFirmId();
+		return userRepository.findById(userId)//
+				.filter(u -> u.isTechUser() || (u.getFirm().getId() == myFirm)) //
+				.map(UserSummary::new) //
+				.orElseThrow(() -> new ResourceNotFoundException("user", "id", userId));
 	}
 
 }
