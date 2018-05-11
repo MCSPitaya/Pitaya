f15ef17396cdc7c04632c0520a5c141d85419105
Andreas Wälchli <andreas.waelchli@me.com>
2018-05-11 11:34:51 +0200

    cleanup

--- src/main/java/ch/pitaya/pitaya/service/CaseService.java
+++ src/main/java/ch/pitaya/pitaya/service/CaseService.java
@@ -1,21 +1,12 @@
 package ch.pitaya.pitaya.service;
 
-import java.util.stream.Stream;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ch.pitaya.pitaya.authorization.AuthCode;
-import ch.pitaya.pitaya.authorization.Authorization;
 import ch.pitaya.pitaya.exception.NotImplementedException;
 import ch.pitaya.pitaya.model.Case;
-import ch.pitaya.pitaya.payload.response.FileSummary;
 
 @Service
 public class CaseService {
-
-	@Autowired
-	private Authorization auth;
 
 	public void patchCase(Case caze) {
 		// TODO: implement
